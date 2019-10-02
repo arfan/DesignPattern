320250a810b97269a186d17a26a18e8ad9686933
Abdul Arfan <abdul.arfan@gmail.com>
2019-10-02 23:43:09 +0700

    adding interface obervable

---
+++ src/xyz/kmbmicro/designpattern/observerpattern/IObservable.java
@@ -0,0 +1,7 @@
+package xyz.kmbmicro.designpattern.observerpattern;
+
+public interface IObservable {
+    void add(IObserver observer);
+    void remove(IObserver observer);
+    void notifyObserver();
+}
---
+++ src/xyz/kmbmicro/designpattern/observerpattern/IObserver.java
@@ -0,0 +1,5 @@
+package xyz.kmbmicro.designpattern.observerpattern;
+
+public interface IObserver {
+    void update();
+}
